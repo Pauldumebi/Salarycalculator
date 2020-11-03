b8a6b5c5ccf801cf5d354f26e33d56a4927c75be
Chukwurah Paul <paulchukwurah7@gmail.com>
2020-11-03 03:40:36 +0100

    fixed negative and higher numbers than 24

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ document.querySelector('.submit').addEventListener('click', function() {
     fname = document.querySelector('.name').value;
     hour =  document.querySelector('.hours').value
     //Number entered should not exceed 24 because there are only 24hrs in a day
-    if (hour > 25) {
-        alert("please enter a valid hour")
-    } else if (hour > 4) {
-        //for overtime
+    if (hour > 24) {
+        alert("please enter a valid hour between 1-24")
+        } else if (hour < 1) {
+            alert("please enter a valid hour between 1-24")
+        }
+         else if (hour > 4) {
+            //for overtime
             overtime = (hour-4)*200,
             normalPayment = hour*1000,
             totalpayment = overtime + normalPayment;
-    } else {
-        totalpayment = hour*1000
-    }
-
-    if (totalpayment <= 4000) {
-        document.querySelector('.totalpayment').textContent = ` ${fname} your total payment for the day is ${totalpayment}  naira`
-       
-    } else  {
-         document.querySelector('.totalpayment').textContent = ` ${fname} your total payment for the day is ₦${totalpayment}  naira`
-          document.querySelector('.overtime').textContent = ` Including your overtime payment of ₦${overtime}`
+        } else {
+            totalpayment = hour*1000
+        if (totalpayment <= 4000) {
+            document.querySelector('.totalpayment').textContent = ` ${fname} your total payment for the day is ${totalpayment}  naira`
+        
+        } else  {
+            document.querySelector('.totalpayment').textContent = ` ${fname} your total payment for the day is ₦${totalpayment}  naira`
+            document.querySelector('.overtime').textContent = ` Including your overtime payment of ₦${overtime}`
+        }
     }
  
 })
