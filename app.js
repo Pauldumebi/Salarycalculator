76f7e2eca6a6506b62bfc554520539facbb422a8
Chukwurah Paul <paulchukwurah7@gmail.com>
2020-11-03 04:32:35 +0100

    fixed calculation bug

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ document.querySelector('.submit').addEventListener('click', function() {
     fname = document.querySelector('.name').value;
     hour =  document.querySelector('.hours').value
     //Number entered should not exceed 24 because there are only 24hrs in a day
-    if (hour > 24) {
+        if (hour < 1) {
         alert("please enter a valid hour between 1-24")
-        } else if (hour < 1) {
+        } else if (hour > 24) {
             alert("please enter a valid hour between 1-24")
-        }
-         else if (hour > 4) {
+        } else if (hour > 4) {
             //for overtime
             overtime = (hour-4)*200,
             normalPayment = hour*1000,
             totalpayment = overtime + normalPayment;
         } else {
             totalpayment = hour*1000
+        }
         if (totalpayment <= 4000) {
             document.querySelector('.totalpayment').textContent = ` ${fname} your total payment for the day is ${totalpayment}  naira`
         
@@ -22,7 +22,7 @@ document.querySelector('.submit').addEventListener('click', function() {
             document.querySelector('.totalpayment').textContent = ` ${fname} your total payment for the day is ₦${totalpayment}  naira`
             document.querySelector('.overtime').textContent = ` Including your overtime payment of ₦${overtime}`
         }
-    }
+    
  
 })
 
